691ac8d2431832ed0193ecb29b711ded80e757ae
puhui999 <puhui999@163.com>
2023-11-21 09:20:32 +0800

    代码生成：新增树表生成示例

---
+++ src/api/infra/demo02.js
@@ -0,0 +1,53 @@
+import request from '@/utils/request'
+
+// 创建示例分类
+export function createDemo02Category(data) {
+  return request({
+    url: '/infra/demo02-category/create',
+    method: 'post',
+    data: data
+  })
+}
+
+// 更新示例分类
+export function updateDemo02Category(data) {
+  return request({
+    url: '/infra/demo02-category/update',
+    method: 'put',
+    data: data
+  })
+}
+
+// 删除示例分类
+export function deleteDemo02Category(id) {
+  return request({
+    url: '/infra/demo02-category/delete?id=' + id,
+    method: 'delete'
+  })
+}
+
+// 获得示例分类
+export function getDemo02Category(id) {
+  return request({
+    url: '/infra/demo02-category/get?id=' + id,
+    method: 'get'
+  })
+}
+
+// 获得示例分类列表
+export function getDemo02CategoryList(params) {
+  return request({
+    url: '/infra/demo02-category/list',
+    method: 'get',
+    params
+  })
+}
+// 导出示例分类 Excel
+export function exportDemo02CategoryExcel(params) {
+  return request({
+    url: '/infra/demo02-category/export-excel',
+    method: 'get',
+    params,
+    responseType: 'blob'
+  })
+}
---
+++ src/views/infra/demo/demo02/index.vue
@@ -0,0 +1,176 @@
+<template>
+  <div class="app-container">
+
+    <!-- 搜索工作栏 -->
+    <el-form :model="queryParams" ref="queryForm" size="small" :inline="true" v-show="showSearch" label-width="68px">
+      <el-form-item label="名字" prop="name">
+        <el-input v-model="queryParams.name" placeholder="请输入名字" clearable @keyup.enter.native="handleQuery"/>
+      </el-form-item>
+      <el-form-item label="父级编号" prop="parentId">
+        <el-input v-model="queryParams.parentId" placeholder="请输入父级编号" clearable @keyup.enter.native="handleQuery"/>
+      </el-form-item>
+      <el-form-item label="创建时间" prop="createTime">
+        <el-date-picker v-model="queryParams.createTime" style="width: 240px" value-format="yyyy-MM-dd HH:mm:ss" type="daterange"
+                        range-separator="-" start-placeholder="开始日期" end-placeholder="结束日期" :default-time="['00:00:00', '23:59:59']" />
+      </el-form-item>
+      <el-form-item>
+        <el-button type="primary" icon="el-icon-search" @click="handleQuery">搜索</el-button>
+        <el-button icon="el-icon-refresh" @click="resetQuery">重置</el-button>
+      </el-form-item>
+    </el-form>
+
+    <!-- 操作工具栏 -->
+    <el-row :gutter="10" class="mb8">
+      <el-col :span="1.5">
+        <el-button type="primary" plain icon="el-icon-plus" size="mini" @click="openForm(undefined)"
+                   v-hasPermi="['infra:demo02-category:create']">新增</el-button>
+      </el-col>
+      <el-col :span="1.5">
+        <el-button type="warning" plain icon="el-icon-download" size="mini" @click="handleExport" :loading="exportLoading"
+                   v-hasPermi="['infra:demo02-category:export']">导出</el-button>
+      </el-col>
+      <el-col :span="1.5">
+        <el-button type="danger" plain icon="el-icon-sort" size="mini" @click="toggleExpandAll">
+          展开/折叠
+        </el-button>
+      </el-col>
+      <right-toolbar :showSearch.sync="showSearch" @queryTable="getList"></right-toolbar>
+    </el-row>
+
+    <el-table
+      v-loading="loading"
+      :data="list"
+      :stripe="true"
+      :show-overflow-tooltip="true"
+      v-if="refreshTable"
+      row-key="id"
+      :default-expand-all="isExpandAll"
+      :tree-props="{children: 'children', hasChildren: 'hasChildren'}"
+    >
+      <el-table-column label="编号" align="center" prop="id" />
+      <el-table-column label="名字" align="center" prop="name" />
+      <el-table-column label="父级编号" align="center" prop="parentId" />
+      <el-table-column label="创建时间" align="center" prop="createTime" width="180">
+        <template v-slot="scope">
+          <span>{{ parseTime(scope.row.createTime) }}</span>
+        </template>
+      </el-table-column>
+      <el-table-column label="操作" align="center" class-name="small-padding fixed-width">
+        <template v-slot="scope">
+          <el-button size="mini" type="text" icon="el-icon-edit" @click="openForm(scope.row.id)"
+                     v-hasPermi="['infra:demo02-category:update']">修改</el-button>
+          <el-button size="mini" type="text" icon="el-icon-delete" @click="handleDelete(scope.row)"
+                     v-hasPermi="['infra:demo02-category:delete']">删除</el-button>
+        </template>
+      </el-table-column>
+    </el-table>
+    <!-- 分页组件 -->
+    <pagination v-show="total > 0" :total="total" :page.sync="queryParams.pageNo" :limit.sync="queryParams.pageSize"
+                @pagination="getList"/>
+
+    <!-- 对话框(添加 / 修改) -->
+    <Demo02CategoryForm ref="formRef" @success="getList" />
+  </div>
+</template>
+
+<script>
+import * as Demo02CategoryApi from '@/api/infra/demo02';
+import Demo02CategoryForm from './Demo02CategoryForm.vue';
+export default {
+  name: "Demo02Category",
+  components: {
+    Demo02CategoryForm,
+  },
+  data() {
+    return {
+      // 遮罩层
+      loading: true,
+      // 导出遮罩层
+      exportLoading: false,
+      // 显示搜索条件
+      showSearch: true,
+      // 总条数
+      // 示例分类列表
+      list: [],
+      // 是否展开，默认全部展开
+      isExpandAll: true,
+      // 重新渲染表格状态
+      refreshTable: true,
+      // 选中行
+      currentRow: {},
+      // 查询参数
+      queryParams: {
+        name: null,
+        parentId: null,
+        createTime: [],
+      }
+    };
+  },
+  created() {
+    this.getList();
+  },
+  methods: {
+    /** 查询列表 */
+    getList() {
+      try {
+        this.loading = true;
+        Demo02CategoryApi.getDemo02CategoryList(this.queryParams).then(response => {
+          this.list = this.handleTree(response.data, 'id', 'parentId');
+        })
+      } finally {
+        this.loading = false;
+      }
+    },
+    /** 搜索按钮操作 */
+    handleQuery() {
+      this.queryParams.pageNo = 1;
+      this.getList();
+    },
+    /** 重置按钮操作 */
+    resetQuery() {
+      this.resetForm("queryForm");
+      this.handleQuery();
+    },
+    /** 添加/修改操作 */
+    openForm(id) {
+      this.$refs["formRef"].open(id)
+    },
+    /** 删除按钮操作 */
+    handleDelete(row) {
+      const that = this;
+      try {
+        const id = row.id;
+        this.$modal.confirm('是否确认删除示例分类编号为"' + id + '"的数据项?').then(()=>{
+          return Demo02CategoryApi.deleteDemo02Category(id);
+        }).then(() => {
+          that.getList();
+          that.$modal.msgSuccess("删除成功");
+        }).catch(() => {});
+      } catch {}
+    },
+    /** 导出按钮操作 */
+    handleExport() {
+      const that = this;
+      try {
+        this.$modal.confirm('是否确认导出所有示例分类数据项?').then(() => {
+          that.exportLoading = true;
+          return Demo02CategoryApi.exportDemo02CategoryExcel(params);
+        }).then(response => {
+          that.$download.excel(response, '示例分类.xls');
+        });
+      } catch {
+      } finally {
+        that.exportLoading = false;
+      }
+    },
+    /** 展开/折叠操作 */
+    toggleExpandAll() {
+      this.refreshTable = false
+      this.isExpandAll = !this.isExpandAll
+      this.$nextTick(function () {
+        this.refreshTable = true
+      })
+    }
+  }
+};
+</script>
