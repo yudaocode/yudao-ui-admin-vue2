3a149a783e71bfefcaa64a187e5914a55b01f8c7
YunaiV <zhijiantianya@gmail.com>
2024-07-17 09:37:15 +0800

    完善 README 文档

---
+++ src/views/ai/chat/manager/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="app-container">
+    <doc-alert title="AI 聊天对话" url="https://doc.iocoder.cn/ai/chat/" />
+
+    <el-link type="danger" target="_blank" href="https://github.com/yudaocode/yudao-ui-admin-vue3">
+      该功能支持 Vue3 + element-plus 版本！
+    </el-link>
+    <br />
+    <el-text>
+      可参考 https://github.com/yudaocode/yudao-ui-admin-vue3/blob/master/src/views/ai/chat/manager/index.vue 代码，pull request 贡献给我们！
+    </el-text>
+  </div>
+</template>
+<script>
+</script>
---
+++ src/views/ai/write/index/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="app-container">
+    <doc-alert title="AI 写作助手" url="https://doc.iocoder.cn/ai/write/" />
+
+    <el-link type="danger" target="_blank" href="https://github.com/yudaocode/yudao-ui-admin-vue3">
+      该功能支持 Vue3 + element-plus 版本！
+    </el-link>
+    <br />
+    <el-text>
+      可参考 https://github.com/yudaocode/yudao-ui-admin-vue3/blob/master/src/views/ai/write/index/index.vue 代码，pull request 贡献给我们！
+    </el-text>
+  </div>
+</template>
+<script>
+</script>
