3627fd8d5402d572b0bfff7f98e3dab76f4de31e
YunaiV <zhijiantianya@gmail.com>
2024-07-27 22:15:15 +0800

    【功能占位】system：三方登录的管理

---
+++ src/views/system/social/user/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="app-container">
+    <doc-alert
+      title="三方登录"
+      url="https://doc.iocoder.cn/social-user/"
+    />
+
+    <el-link type="danger" target="_blank" href="https://github.com/yudaocode/yudao-ui-admin-vue3">
+      该功能支持 Vue3 + element-plus 版本！
+    </el-link>
+    <br />
+    <el-text>
+      可参考 https://github.com/yudaocode/yudao-ui-admin-vue3/blob/master/src/views/system/social/user/index.vue 代码，pull request 贡献给我们！
+    </el-text>
+  </div>
+</template>
+<script>
+</script>
