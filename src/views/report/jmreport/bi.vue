b030289314a03d33f39c7d09b2f63c6bd11954ba
YunaiV <zhijiantianya@gmail.com>
2025-05-03 10:37:00 +0800

    feat：集成积木仪表盘

---
+++ src/views/report/jmreport/bi.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <doc-alert title="大屏设计器" url="https://doc.iocoder.cn/screen/" />
+    <i-frame :src="url" />
+  </div>
+</template>
+<script>
+import iFrame from "@/components/iFrame/index";
+import {getAccessToken} from "@/utils/auth";
+export default {
+  name: "JimuBI",
+  components: { iFrame },
+  data() {
+    return {
+      url: process.env.VUE_APP_BASE_API + "/drag/list?token=" + getAccessToken(),
+    };
+  },
+};
+</script>
